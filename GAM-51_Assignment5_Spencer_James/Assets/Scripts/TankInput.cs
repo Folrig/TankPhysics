c9232bc9dd0d9a849f69821ef0c40d8c1d6990b3
James <jspencer30@student.rccd.edu>
2019-04-29 17:53:53 -0700

    Initial asset file dump
    Project start asset file dump

---
+++ GAM-51_Assignment5_Spencer_James/Assets/Scripts/TankInput.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankInput : MonoBehaviour
+{
+    #region Variables
+    private TankPhysics tankPhysics;
+    #endregion
+
+    #region Methods
+    void Start()
+    {
+        tankPhysics = GetComponent<TankPhysics>();
+    }
+
+    void Update()
+    {
+        tankPhysics.acceleration = Input.GetAxis("Vertical");
+        tankPhysics.braking = Input.GetAxis("Jump");
+        tankPhysics.steering = Input.GetAxis("Horizontal");
+    }
+    #endregion
+}
---
+++ GAM-51_Assignment5_Spencer_James/Assets/Scripts/TankPhysics.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankPhysics : MonoBehaviour
+{
+    #region Variables
+    [SerializeField] private Rigidbody rb;
+    private Vector3 massOffset = new Vector3(0f, -0.25f, 0f);
+    public float acceleration = 0f;
+    public float braking = 0f;
+    public float steering = 0f;
+    [SerializeField] private float enginePower = 10.0f;
+    [SerializeField] private float brakingPower = 5.0f;
+    #endregion
+
+    #region Methods
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.centerOfMass = massOffset;
+	}
+
+	void FixedUpdate()
+    {
+        rb.AddForce(Vector3.forward * enginePower, ForceMode.Force);
+        rb.AddForce(Vector3.back * brakingPower, ForceMode.Force);
+	}
+    #endregion
+}
